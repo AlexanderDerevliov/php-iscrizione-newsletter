cb0ae230c4a140312cee8cefbcf4dfc5bdcae86d
AlexanderDerevliov <alex.feelred@gmail.com>
2023-12-14 16:41:43 +0100

    add Boostrap to row and column

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
+<!-- PHP LOGIC -->
+<?php
+
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,23 +18,32 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Newsletter</title>
 </head>
+
 <body>
     <main>
-        <div class="container">
+        <div class="container justify-content-center">
             <!-- title -->
-            <h3>Isciviti alla NewsLetter</h3>
+            <h3 class="text-center mt-5">Isciviti alla NewsLetter</h3>
 
             <!-- form -->
-            <form action="">
-                <label for="email">Inserisci qui la tua e-mail</label>
-                <input class="form-control mb-4" id="email" name="email" type="text">
+            <div class="row">
+                <div class="col-5">
+                    <form action="index.php" method="POST">
+                        <label for="email">Inserisci qui la tua e-mail</label>
+                        <input class="form-control mb-4" id="email" name="email" type="text">
 
-                <!-- button to send form -->
+                        <!-- button to send form -->
 
-                <button class="btn btn-primary" type="submit">Invia</button>
+                        <button class="btn btn-primary text-center" type="submit">Invia</button>
+                </div>
+
+
+            </div>
+
             </form>
         </div>
     </main>
-    
+
 </body>
+
 </html>
